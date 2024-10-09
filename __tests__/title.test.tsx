06ad18682a25028d72401a0d3fbcddd061f7986f
hirohiro-sys <katohiro8843@icloud.com>
2024-10-09 20:22:02 +0900

    firebase toolsを最新に

--- __tests__/title.test.tsx
+++ __tests__/title.test.tsx
@@ -1,6 +1,7 @@
 import { render, screen } from "@testing-library/react";
 import Title from "../app/components/title";
 
+// jestが動いてるか確認するようのテスト
 describe("テストが動くかのテスト", () => {
   test("titleが表示される", () => {
     render(<Title />);
