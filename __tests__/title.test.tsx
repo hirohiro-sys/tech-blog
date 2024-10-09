ebf14914c6dcd1b3cb5763775c602c9c388e6b52
hirohiro-sys <katohiro8843@icloud.com>
2024-10-09 21:03:25 +0900

    secrets修正

--- __tests__/title.test.tsx
+++ __tests__/title.test.tsx
@@ -1,7 +1,6 @@
 import { render, screen } from "@testing-library/react";
 import Title from "../app/components/title";
 
-// jestが動いてるか確認するようのテスト
 describe("テストが動くかのテスト", () => {
   test("titleが表示される", () => {
     render(<Title />);
