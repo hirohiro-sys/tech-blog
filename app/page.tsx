48f9806e89853928dcaa86e9a1f2357643065d91
hirohiro-sys <katohiro8843@icloud.com>
2024-10-13 13:37:54 +0900

    謎

--- app/page.tsx
+++ app/page.tsx
@@ -3,20 +3,14 @@ import QiitaPage from "./components/qiitaPage";
 import MicrocmsPage from './components/microcmsPage';
 
 export default async function Page() {
-  const headersList = headers();
-  const host = headersList.get('host');
+  const headersData = headers()
+  const host = headersData.get('host')
+  const protocol = headersData.get('x-forwarded-proto') ?? host.startsWith('localhost') ? 'http' : 'https'
+  const apiBase = `${protocol}://${host}`
 
-  // プロトコルの取得: X-Forwarded-Proto ヘッダーから取得する
-  const protocol = headersList.get('x-forwarded-proto') || 'http';
-  // console.log(`protocol: ${protocol}`);
+  const data = await fetch(`${apiBase}/api/qiita`, { cache: "no-store" }).then(data => data.json());
 
-  const res = await fetch(`${protocol}://${host}/api/qiita`, { cache: "no-store" });
-
-  if (!res.ok) {
-    throw new Error(`Fetch failed with status: ${res.status}`);
-  }
-
-  const data = await res.json();
+  // const data = await res.json();
 
   return (
     <div className="px-5 py-5">
