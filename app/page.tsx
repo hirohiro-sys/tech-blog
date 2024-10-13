30e54ad12c8fe382925f263235e2c659b17ae7b7
hirohiro-sys <katohiro8843@icloud.com>
2024-10-13 13:51:06 +0900

    一旦寝る

--- app/page.tsx
+++ app/page.tsx
@@ -1,19 +1,25 @@
 import { headers } from 'next/headers';
-import QiitaPage from "./components/qiitaPage";
+import QiitaPage from './components/qiitaPage';
 import MicrocmsPage from './components/microcmsPage';
 
 export default async function Page() {
-  const headersData = headers()
-  const host = headersData.get('host')
-  const protocol = headersData.get('x-forwarded-proto') ?? host.startsWith('localhost') ? 'http' : 'https'
-  const apiBase = `${protocol}://${host}`
+  const headersData = headers();
+  
+  const protocol = headersData.get('x-forwarded-proto') || 'http';
+  const host = headersData.get('host');
 
-  const data = await fetch(`${apiBase}/api/qiita`, { cache: "no-store" }).then(data => data.json());
+  const apiBase = `${protocol}://${host}`;
+  const res = await fetch(`${apiBase}/api/qiita`, { cache: "no-store" });
 
-  // const data = await res.json();
+  if (!res.ok) {
+    throw new Error(`Fetch failed with status: ${res.status}`);
+  }
+
+  const data = await res.json();
 
   return (
     <div className="px-5 py-5">
+      {/* データを表示するコンポーネント */}
       <QiitaPage data={data} />
       <MicrocmsPage />
     </div>
