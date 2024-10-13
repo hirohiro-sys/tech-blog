fed2231a0b5f66be27095119c71c21d1beb4e84e
hirohiro-sys <katohiro8843@icloud.com>
2024-10-13 13:19:42 +0900

    jsonが返ってない？

--- app/page.tsx
+++ app/page.tsx
@@ -6,7 +6,16 @@ export default async function Page() {
   const headersList = headers();
   const host = headersList.get('host');
 
-  const res = await fetch(`http://${host}/api/qiita`, { cache: "no-store" });
+  // プロトコルの取得: X-Forwarded-Proto ヘッダーから取得する
+  const protocol = headersList.get('x-forwarded-proto') || 'http';
+  // console.log(`protocol: ${protocol}`);
+
+  const res = await fetch(`${protocol}://${host}/api/qiita`, { cache: "no-store" });
+
+  if (!res.ok) {
+    throw new Error(`Fetch failed with status: ${res.status}`);
+  }
+
   const data = await res.json();
 
   return (
